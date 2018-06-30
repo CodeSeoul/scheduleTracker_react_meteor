aedb927b504683dd3097e6a878c1a5d19f92150b
Alex <rollinglex@gmail.com>
2018-07-01 03:00:23 +0900

    global reset

---
+++ imports/ui/globalStyles.js
@@ -0,0 +1,13 @@
+import { injectGlobal } from 'styled-components';
+import reset from 'styled-reset';
+//resets styles
+injectGlobal`
+  ${reset}
+
+  body {
+    margin: 0;
+    border: 0;
+    padding: 0;
+    font-family: 'Roboto';
+  }
+`;
