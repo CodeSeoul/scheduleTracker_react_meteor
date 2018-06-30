b4fdeca573932a3380396704c38dc1fff5a7dcbb
Alex <rollinglex@gmail.com>
2018-07-01 02:59:13 +0900

    setup dir structure of React

---
+++ imports/ui/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+// App component - represents the whole app
+class App extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Schedule Tracker</h1>
+        </header>
+
+      </div>
+    );
+  }
+}
+
+export default App
