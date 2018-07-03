bd006228ebcdc215c7ce074ac9da70ef3979ccfe
Dasol-Jung <xenosophian@gmail.com>
2018-07-03 23:53:57 +0900

    put section, name, rank, and days data in Member component

--- imports/ui/components/Member/Member.js
+++ imports/ui/components/Member/Member.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { Fragment } from "react";
 
-const Member = (props)=>{
+const Member = props => {
+  const weeklySchedule = props.employee.schedule[0].map(dailySchedule => {
+    return <div key={props.key}>{dailySchedule}</div>;
+  });
 
-    return(
-
-        <div>
-        </div>
-
-    )
-}
+  return (
+    <Fragment>
+      <div>{props.employee.section}</div>
+      <div>{props.employee.firstName}</div>
+      <div>{props.employee.section}</div>
+      {weeklySchedule}
+    </Fragment>
+  );
+};
 
 export default Member;
