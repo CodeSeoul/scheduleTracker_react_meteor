7013f49355adb38948fce38034ca31bb19ba6003
Joel Quek <joel.qcx@gmail.com>
2018-07-04 20:09:09 +0900

    added dashboard and login button

--- imports/ui/components/dashboard/Dashboard.js
+++ imports/ui/components/dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const Dashboard = (props)=>{
 
-    const ButtonContainer = styled.button`
+    const WeekButtonContainer = styled.button`
         background-color:#81baea;
         border: none;
         padding: 10px;
@@ -12,7 +12,6 @@ const Dashboard = (props)=>{
         height:100px;
         box-shadow: 0 -2px 5px 3px #659ac6 inset, 0 5px 5px rgba(3, 25, 41, 0.17), 0 15px rgba(255, 255, 255, 0.25) inset;
         font-size: 20px;
-        font-family: Verdana, Geneva, Tahoma, sans-serif;
         text-align: center;
         text-decoration: none;
         display: inline-block;
@@ -20,13 +19,27 @@ const Dashboard = (props)=>{
         text-shadow: 2px 2px #818181;
         cursor:pointer;
     `
+    const GeneralButtonContainer = styled.button`
+        border:none;
+        font-size: 20px;
+        width:auto;
+        height:auto;
+        background: none;
+        cursor:pointer;
+    `
 
     return(
-
-        <ButtonContainer>
-            Week 1
-        </ButtonContainer>
-
+        <div>
+            <WeekButtonContainer>
+                Week 1
+            </WeekButtonContainer>
+            <GeneralButtonContainer>
+                Dashboard
+            </GeneralButtonContainer>
+            <GeneralButtonContainer>
+            Log in
+            </GeneralButtonContainer>
+        </div> 
     )
 }
 
