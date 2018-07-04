7be394abea4fea5cafd63fa832b7e3776e026afd
Dasol-Jung <xenosophian@gmail.com>
2018-07-04 14:36:10 +0900

    built Dashboard component basic structure

--- imports/ui/components/dashboard/Dashboard.js
+++ imports/ui/components/dashboard/Dashboard.js
@@ -5,6 +5,17 @@ const Dashboard = (props)=>{
     return(
 
         <div>
+            <div>Logo</div>
+
+            <button>Week1</button>
+
+            <nav>
+                <ul>
+                    <li><a href='#'>Dashboard</a></li>
+                    <li><a href='#'>Log in</a></li>
+                    <li><a href='#'>Log out</a></li>
+                </ul>
+            </nav>
         </div>
 
     )
