4d1e91a774672d668074b20c0f6a471606513cdd
Alex <rollinglex@gmail.com>
2018-07-06 18:46:40 +0900

    abstract Modal out of ModalContainer.js

--- imports/ui/components/helpers/Modals/ModalContainer.js
+++ imports/ui/components/helpers/Modals/ModalContainer.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import DashboardModal from './DashboardModal';
-import LoginModal from './LoginModal';
-import WeekModal from './WeekModal';
+import Modal from './Modal';
 
 const ModalContainer = ({ showModal, toggleModalHandler, modal }) => {
   const showHideClassName = showModal
     ? 'modal display-block'
     : 'modal display-none';
-  const modalDisplayed = () => {
-    switch (modal) {
-      case 'week':
-        return WeekModal;
-      case 'dashboard':
-        return DashboardModal;
-      default:
-        return LoginModal;
-    }
-  };
+
   return (
     <div className={showHideClassName}>
       <section className="modal-main">
-        modalDisplayed
+        <Modal modal={modal} />
         <button onClick={toggleModalHandler}>Close</button>
       </section>
     </div>
