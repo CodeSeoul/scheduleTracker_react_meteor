407163f2aa5541c64dbb0f7c4195e2c3269743b9
joelquek <joel.qcx@gmail.com>
2018-07-07 20:18:05 +0900

    moved buttons style to dashboardstyle.js

--- imports/ui/components/dashboard/DashboardStyle.js
+++ imports/ui/components/dashboard/DashboardStyle.js
@@ -32,4 +32,37 @@ nav{
 
 `
 
-export default Navbar;
+const WeekButton = styled.button`
+  background-color: #81baea;
+  border: none;
+  padding: 10px;
+  border-radius: 50%;
+  width: 100px;
+  height: 100px;
+  box-shadow: 0 -2px 5px 3px #659ac6 inset, 0 5px 5px rgba(3, 25, 41, 0.17),
+    0 15px rgba(255, 255, 255, 0.25) inset;
+  font-size: 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  color: #fafdff;
+  text-shadow: 2px 2px #818181;
+  cursor: pointer;
+  &:hover {
+    background-color: #8dc0ec;
+  }
+`;
+const GeneralButton = styled.button`
+  border: none;
+  font-size: 20px;
+  width: auto;
+  height: auto;
+  background: none;
+  cursor: pointer;
+  &:hover {
+    background-color: #eff6fc;
+  }
+`;
+
+
+export default { Navbar, WeekButton, GeneralButton } ;
