d3fb33d4a395c24ec5bcc9eccfa54eb4f2d11d59
Dasol-Jung <xenosophian@gmail.com>
2018-07-07 11:32:55 +0900

    made schedule days cells change color according to their value

--- imports/ui/components/Member/Member.js
+++ imports/ui/components/Member/Member.js
@@ -1,20 +1,21 @@
 import React, { Fragment } from "react";
+import { FixedColumn, Cell } from './MemberStyle'
 
 const Member = props => {
 
   //I'm going to add 'week' key to the state in App component and pass it to Member component. For now, I'm using week 0(index '0')
   const { schedule, _id, section, firstName, rank } = props
   const weeklySchedule = schedule[0].map((dailySchedule, index) => {
-    return <Cell key={_id + index}>{dailySchedule}</Cell>;
+    return <Cell type={dailySchedule} key={_id + index}>{dailySchedule}</Cell>;
   });
 
 
 
   return (
     <Fragment>
-      <div>{section}</div>
-      <div>{firstName}</div>
-      <div>{rank}</div>
+      <FixedColumn>{section}</FixedColumn>
+      <FixedColumn>{firstName}</FixedColumn>
+      <FixedColumn>{rank}</FixedColumn>
       {weeklySchedule}
     </Fragment>
   );
--- imports/ui/components/Member/MemberStyle.js
+++ imports/ui/components/Member/MemberStyle.js
@@ -1,8 +1,32 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-const Cell = styled.div`
-    
+const FixedColumn = styled.div`
+  width: 100%;
+  height: 100%;
+  display: grid;
+  justify-items: center;
+  align-items: center;
+  border: 0.05rem #ccc solid;
+`;
 
-`
+const Cell = FixedColumn.extend`
+  width: 95%;
+  height: 95%;
 
-export default Cell;
+  ${props => {
+    switch (props.type) {
+      case 0:
+        return "background:linear-gradient(rgba(255,255,255,100%),rgba(255,255,255,100%) 20%, rgba(195,36,186,0.7));";
+      case 1:
+        return "background:linear-gradient(rgba(255,255,255,100%),rgba(255,255,255,100%) 20%, rgba(210,13,13,0.7));";
+      case 2:
+        return "background:linear-gradient(rgba(255,255,255,100%),rgba(255,255,255,100%) 20%, rgba(35,183,255,0.7));";
+      case 3:
+        return "background:linear-gradient(rgba(255,255,255,100%),rgba(255,255,255,100%) 20%, rgba(21,110,23,0.7));";
+      case 4:
+        return "background:linear-gradient(rgba(255,255,255,100%),rgba(255,255,255,100%) 20%, rgba(255,163,13,0.7));";
+    }
+  }};
+`;
+
+export { Cell, FixedColumn };
