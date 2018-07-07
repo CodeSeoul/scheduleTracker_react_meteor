06c8049956a960c013920faaee6d8a9c22120329
Dasol-Jung <xenosophian@gmail.com>
2018-07-07 10:19:01 +0900

    made weeklySchedule function return a Cell component instead of a div element

--- imports/ui/components/Member/Member.js
+++ imports/ui/components/Member/Member.js
@@ -5,7 +5,7 @@ const Member = props => {
   //I'm going to add 'week' key to the state in App component and pass it to Member component. For now, I'm using week 0(index '0')
   const { schedule, _id, section, firstName, rank } = props
   const weeklySchedule = schedule[0].map((dailySchedule, index) => {
-    return <div key={_id + index}>{dailySchedule}</div>;
+    return <Cell key={_id + index}>{dailySchedule}</Cell>;
   });
 
 
