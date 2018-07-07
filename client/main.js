4f0fcb405430a1d53666759b9e8c42917e1e552c
Epikem <dltldls95@naver.com>
2018-07-07 22:58:34 +0900

    add login route to main

--- client/main.js
+++ client/main.js
@@ -4,12 +4,14 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import App from '../imports/ui/App.js';
+import { LoginPage } from '../imports/ui/components/LoginPage';
 
 Meteor.startup(() => {
   render(
   <Router>
     <div>
       <Switch>
+        <Route path='/login' component={LoginPage}/>
         <Route path='/' component={App}/>
       </Switch>
     </div>
