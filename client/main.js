8da16603683aebc1759fe5bd3b9cc87b287a3610
Epikem <dltldls95@naver.com>
2018-07-07 22:48:59 +0900

    add App route to main

--- client/main.js
+++ client/main.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import App from '../imports/ui/App.js';
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('render-target'));
+  render(
+  <Router>
+    <div>
+      <Switch>
+        <Route path='/' component={App}/>
+      </Switch>
+    </div>
+  </Router>
+  , document.getElementById('render-target'));
 });
