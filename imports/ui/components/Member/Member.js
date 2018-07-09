4e0466091ec0eb91d782785eeb4669cc98885d80
Dasol-Jung <xenosophian@gmail.com>
2018-07-09 22:05:14 +0900

    Changed daily schedule number code into status string

--- imports/ui/components/Member/Member.js
+++ imports/ui/components/Member/Member.js
@@ -4,18 +4,15 @@ import { FixedColumn, Select, Option } from '../../styles/MemberStyle'
 const Member = props => {
 
   //I'm going to add 'week' key to the state in App component and pass it to Member component. For now, I'm using week 0(index '0')
-  const { schedule, _id, section, firstName, rank } = props
+  const { schedule, _id, section, firstName, rank, status } = props
   const weeklySchedule = schedule[0].map((dailySchedule, index) => {
     return (
       <Select type={dailySchedule} key={_id + index}>
-        <Option>{dailySchedule}</Option>
-        <Option>{dailySchedule}</Option>
-        <Option>{dailySchedule}</Option>
-        <Option>{dailySchedule}</Option>
+        {status.map((stat,idx)=>{return <Option key = {_id + idx} selected={dailySchedule===idx ? 'selected':''}>{stat}</Option> })}
       </Select>
       );
   });
-
+ 
 
 
   return (
