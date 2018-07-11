f52aade47487f1c72646debecec50219f7ab6e36
Dasol-Jung <xenosophian@gmail.com>
2018-07-11 20:19:31 +0900

    refactored Member.js

--- imports/ui/components/Member/Member.js
+++ imports/ui/components/Member/Member.js
@@ -1,21 +1,38 @@
 import React, { Fragment } from "react";
-import { FixedColumn, Select, Option } from '../../styles/MemberStyle'
+import { FixedColumn, Select, Option } from "../../styles/MemberStyle";
 
 const Member = props => {
-
   //I'm going to add 'week' key to the state in App component and pass it to Member component. For now, I'm using week 0(index '0')
-  const { schedule, _id, section, lastName, firstName, Rank, Section, rank, status } = props
+  const {
+    schedule,
+    _id,
+    section,
+    lastName,
+    firstName,
+    Rank,
+    Section,
+    rank,
+    status
+  } = props;
   const weeklySchedule = schedule[0].map((dailySchedule, index) => {
     return (
-      <Select type={dailySchedule} key={_id + index}>
-        {status.map((stat,idx)=>{return <Option key = {_id + idx} selected={dailySchedule===idx ? 'selected':''}>{stat}</Option> })}
+      <Select
+        type={dailySchedule}
+        key={_id + index}>
+          {status.map((stat, idx) => {
+            return (
+              <Option
+                key={_id + idx}
+                selected={dailySchedule === idx ? "selected" : ""}>
+                {stat}
+              </Option>
+            );
+          })}
       </Select>
-      );
+    );
   });
 
   console.log(props);
- 
-
 
   return (
     <Fragment>
