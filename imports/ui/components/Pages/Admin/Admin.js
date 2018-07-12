d2b2ca0a6d1721dd56d99c98802a991dd10704c2
Alex <rollinglex@gmail.com>
2018-07-12 19:55:59 +0900

    error handling for adding new Employee

--- imports/ui/components/Pages/Admin/Admin.js
+++ imports/ui/components/Pages/Admin/Admin.js
@@ -3,10 +3,43 @@ import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 
 class Admin extends React.Component {
+  state = {
+    errors: []
+  };
+  checkInputs = () => {
+    let errors = [];
+    if (this.password.value !== this.confirmPassword.value) {
+      errors = [...errors, 'Password does not match'];
+    }
+    if (this.password.value.length < 1) {
+      errors = [...errors, 'Password is required'];
+    }
+    if (this.username.value.length < 1) {
+      errors = [...errors, 'Username is required'];
+    }
+
+    if (this.username.value.length < 3) {
+      console.log('less than 3', this.username.value);
+      errors = [...errors, 'Username must be at least 3 characters long'];
+    }
+    if (errors.length > 0) {
+      console.log('error set state', errors);
+      this.setState({
+        errors: errors
+      });
+      return false;
+    }
+    this.setState({
+      error: []
+    });
+    return true;
+  };
   addNewUserHandler = e => {
     e.preventDefault();
     e.stopPropagation();
-
+    if (!this.checkInputs()) {
+      return false;
+    }
     Accounts.createUser(
       {
         username: this.username.value,
@@ -21,12 +54,20 @@ class Admin extends React.Component {
         }
       }
     );
+    this.username.value = '';
+    this.password.value = '';
+    this.confirmPassword.value = '';
   };
   render() {
+    console.log('this.props', this.props);
     return (
       <div>
         <h2>Admininistrator</h2>
         <main>
+          {this.state.errors.length > 0 &&
+            this.state.errors.map((err, i) => {
+              return <p key={i}>{err}</p>;
+            })}
           <h3>Add Employee</h3>
           <form onSubmit={this.addNewUserHandler}>
             <input
