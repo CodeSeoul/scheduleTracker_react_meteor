bfd076ff776f47873d98fc45c4e77b4201bcc9de
joelquek <joel.qcx@gmail.com>
2018-07-14 08:45:00 +0900

    modified dashboard. wrapped buttons into navbar component

--- imports/ui/components/dashboard/Dashboard.js
+++ imports/ui/components/dashboard/Dashboard.js
@@ -1,39 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
 import '../../../../client/main.css';
 import ModalContainer from '../helpers/Modals/ModalContainer';
-
-const WeekButton = styled.button`
-  background-color: #81baea;
-  border: none;
-  padding: 10px;
-  border-radius: 50%;
-  width: 100px;
-  height: 100px;
-  box-shadow: 0 -2px 5px 3px #659ac6 inset, 0 5px 5px rgba(3, 25, 41, 0.17),
-    0 15px rgba(255, 255, 255, 0.25) inset;
-  font-size: 20px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  color: #fafdff;
-  text-shadow: 2px 2px #818181;
-  cursor: pointer;
-  &:hover {
-    background-color: #8dc0ec;
-  }
-`;
-const GeneralButton = styled.button`
-  border: none;
-  font-size: 20px;
-  width: auto;
-  height: auto;
-  background: none;
-  cursor: pointer;
-  &:hover {
-    background-color: #eff6fc;
-  }
-`;
+import Navbar from './Navbar';
+import { Header, DashboardWrapper } from '../../styles/DashboardStyle';
 
 class Dashboard extends React.Component {
   state = {
@@ -50,24 +19,20 @@ class Dashboard extends React.Component {
 
   render() {
     return (
-      <div>
+      <DashboardWrapper>
         <ModalContainer
           showModal={this.state.showModal}
           toggleModalHandler={this.toggleModalHandler}
           modal={this.state.modal}
         />
 
-        <WeekButton onClick={() => this.toggleModalHandler('week')}>
-          Week 1
-        </WeekButton>
+        <Header>
+          Schedule Tracker
+        </Header>
+      
+        <Navbar toggleModalHandler={this.toggleModalHandler}/>
 
-        <GeneralButton onClick={() => this.toggleModalHandler('dashboard')}>
-          Dashboard
-        </GeneralButton>
-        <GeneralButton onClick={() => this.toggleModalHandler('login')}>
-          Log in
-        </GeneralButton>
-      </div>
+      </DashboardWrapper>
     );
   }
 }
