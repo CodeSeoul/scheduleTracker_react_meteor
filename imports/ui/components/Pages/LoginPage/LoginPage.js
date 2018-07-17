7d72b8ff25f90684bda886d26cb281cee08cc4bb
Epikem <dltldls95@naver.com>
2018-07-18 00:21:40 +0900

    added LoginPage background style

--- imports/ui/components/Pages/LoginPage/LoginPage.js
+++ imports/ui/components/Pages/LoginPage/LoginPage.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { Redirect } from 'react-router';
 import Login from './LoginForm';
 import LoginForm from './LoginForm';
+import { LoginPageBackground } from '../../../styles/LoginPageStyle';
+
 const LoginPage = () => {
   const redirectUser = Meteor.userId() ? <Redirect to="/" /> : null;
   return (
     <div>
+      <LoginPageBackground>
           Schedule Tracker
         <LoginForm />
-      {redirectUser}
-      
+        {redirectUser}
+      </LoginPageBackground>
     </div>
   );
 };
---
+++ imports/ui/styles/LoginPageStyle.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const LoginPageBackground= styled.div`
+    width: 100%;
+    height:100%;
+    position:fixed;
+    top:0;
+    left:0;
+    background: linear-gradient(
+    to bottom,
+    rgba(20, 33, 61, 1),
+    rgba(41, 53, 78, 0.95)
+  );
+`;
+
+export { LoginPageBackground };
