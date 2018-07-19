911067faa159583066a64adee48c879745b26fc3
Epikem <dltldls95@naver.com>
2018-07-19 13:33:44 +0900

    added EditableText to NameContainer

--- imports/ui/components/Member/Member.js
+++ imports/ui/components/Member/Member.js
@@ -71,13 +71,58 @@ class Member extends React.Component {
     }
 
     const NameContainer = ({editable}) => {
-      if(editable) return <div>{`${firstName} ${lastName}`}</div>
+      if(editable) return <EditableText defaultValue={`${firstName} ${lastName}`}/>
       else return <div>{`${firstName} ${lastName}`}</div>
     }
 
     const RankContainer = ({editable}) => {
       if(editable) return <div>{Rank[rank]}</div>
       else return <div>{Rank[rank]}</div>
+    }
+
+    class EditableText extends React.Component{
+      state = {
+        lastValue: '',
+        editingValue: '',
+        onEdit: false,
+      }
+
+      constructor(props){
+        super(props);
+        this.state.lastValue = props.defaultValue;
+      }
+
+      handleEdit = (e) => {
+        this.setState({editingValue: e.target.value});
+      }
+
+      handleEnter = (e) => {
+        if(e.key == 'Enter'){
+          const value = this.state.editingValue;
+          this.setState({onEdit: false, lastValue: value});
+          var firstName = value.substring(0, value.lastIndexOf(' ') + 1);
+          var lastName = value.substring(value.lastIndexOf(' ') + 1, value.length);
+        }
+      }
+
+      handleCancel = (e) => {
+        this.setState({onEdit: false});
+      }
+
+      handleClick = (e) => {
+        this.setState({onEdit: true, editingValue: this.state.lastValue});
+      }
+
+      render = () => {
+        if(this.state.onEdit){
+          return(
+            <input autoFocus type="text" value={this.state.editingValue} onChange={this.handleEdit} onBlur={this.handleCancel} onKeyPress={this.handleEnter}/>
+          )
+        }
+        return (
+          <span onClick={this.handleClick}>{this.state.lastValue}</span>
+        )
+      }
     }
 
     const editable = this.getEditAccess();
