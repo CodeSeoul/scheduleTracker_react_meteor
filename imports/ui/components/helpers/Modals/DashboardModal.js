3effb5a66bc63c003744a719ad1a3a2f9264f7a4
Joel Quek <joel.qcx@gmail.com>
2018-07-26 23:52:08 +0900

    added table for week summary

--- imports/ui/components/helpers/Modals/DashboardModal.js
+++ imports/ui/components/helpers/Modals/DashboardModal.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import {ModalTable,
+        TableData, 
+        TableHead,
+        ModalHeader} from '../../../styles/DashboardModalStyle.js'
 const daysInAWeek = ['Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday' , 'Sunday'];
 const scheduleStatus = ['Normal', 'Training', 'Weekend OT', 'Business Trip', 'On Leave'];
 const DashboardModal = ({employees, week}) => {
@@ -10,25 +14,39 @@ const DashboardModal = ({employees, week}) => {
                 [0,0,0,0,0], //friday
                 [0,0,0,0,0], //saturday
                 [0,0,0,0,0]]; //sunday
-   employees.map((employee,i) =>{
+   
+    employees.map((employee,i) =>{ //count number of statuses in each day
       employee.info.schedule[week-1].map((status, i)=>{
         count[i][status]++;
       }); 
     });
 
-    const summary = count.map((statuses, i) => {
-      return (<div key={i}>{daysInAWeek[i]}: {statuses.map((status,j) => {
-              if(status !== 0 && j!==0){
-                return (scheduleStatus[j] + '>' + status + "person.");
-              }
-            })}</div>)
+    const tblHead = scheduleStatus.map((status,i)=>{
+      return <TableHead key={i}>{status}</TableHead>
+    });
+
+    const tblData = count.map((statusesCount, i) => {
+      return (<tr key={'row-'+i}>
+              <TableData key={'day-'+i}>{daysInAWeek[i]}</TableData> 
+              {statusesCount.map((statusCount,j) => {
+                return (<TableData key={'status-'+j}>{statusCount}</TableData>);
+            })}</tr>)
     });
 
   return (<div>
-          <h1>
-           Week {week}
-          </h1>
-            {summary}
+            <ModalHeader>
+            Week {week}
+            </ModalHeader>
+            <ModalTable>
+              <thead>
+                <tr>
+                  <TableHead>Day</TableHead>{tblHead}
+                </tr>
+              </thead>
+              <tbody>
+                {tblData}
+              </tbody>
+            </ModalTable>
           </div>
           );
 };
